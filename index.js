b01e33b1c41e3d2aa9432ded320309613d54fe82
boogeymanCoder <johnbryanpitcasoy@gmail.com>
2021-10-18 07:56:09 +0800

    fixed server error

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-var data = null;
+var data = "None";
 
 app.get("/", (req, res) => {
-  res.send("Data on Database:", data);
+  res.send(`Data on Database: ${data}`);
 });
 
 app.post("/", (req, res) => {
   data = req.body;
-  res.send("Data Received:", data);
+  res.send(`Data Received: ${data}`);
 });
 
 app.listen(process.env.PORT || 3000);
