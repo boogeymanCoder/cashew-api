fec311abfde29722fe1c1fc6ab754dcde8c52ab3
boogeymanCoder <johnbryanpitcasoy@gmail.com>
2021-10-18 07:38:30 +0800

    usable

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+var data = null;
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json(data);
+});
+
+app.post("/", (req, res) => {
+  data = req.body;
+  res.send(data);
 });
 
 app.listen(process.env.PORT || 3000);
